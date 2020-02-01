97714d975bd02d01d7a0278ca9fee521de9af2c4
Doris Morales <48230115+moralesdoris67@users.noreply.github.com>
2020-02-01 01:15:54 -0500

    Add files via upload

---
+++ objectcollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectcollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "deadplant")
+        {
+            Debug.Log("deadplant");
+            playerscore++; //refrence score variable from scoresystem script
+        }
+
+        if (collision.gameObject.tag == "plastic")
+        {
+            Debug.Log("plastic");
+            Destroy(this.gameObject);
+        }
+    }
+}
