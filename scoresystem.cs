6a8c0120910e7dbb7d9a2025eedbe5a6a26b459b
Doris Morales <48230115+moralesdoris67@users.noreply.github.com>
2020-02-01 17:17:00 -0500

    Add files via upload

---
+++ scoresystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scoresystem : MonoBehaviour
+{
+  public Text scoreText;
+  private float timer;
+  private float score;
+  private int start = 18000;
+
+
+    void Update()
+    {
+
+      int t=start-=1;
+
+      string minutes=((start/60)/60).ToString("00");
+      string seconds =(((int)start/60)/5).ToString("00");
+      string miliseconds=(t%60).ToString("00");
+
+      scoreText.text="Time: "+minutes+":"+seconds+":"+miliseconds;//+miliseconds;
+
+}
+}
